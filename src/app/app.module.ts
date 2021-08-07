b7dce890949d9e015d4932128a711c282ed7f227
TechnoPanda3k <technopanda3k@gmail.com>
2021-08-07 23:20:31 +0530

    App Header and Footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppFooterComponent } from './app-footer/app-footer.component';
+import { AppHeaderComponent } from './app-header/app-header.component';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppHeaderComponent,
+    AppFooterComponent
   ],
   imports: [
     BrowserModule,
